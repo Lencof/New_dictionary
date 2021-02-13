346c29bab57da26b9a87fbfb1b21325275f36779
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-13 18:23:59 +0300

    Update New_dictionary.py

--- New_dictionary.py
+++ New_dictionary.py
@@ -1,3 +1,8 @@
 # __Author__ __Lencof__
 # New_dictionary.py
 
+# create New_dictionary
+New_dictionary = {'Banana': 'Apple',
+              'grapes': 'watermelon',
+              'melon': 'cabbage',
+              'orange': 'pear'}
